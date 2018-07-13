8a5b6600fed13fb090a35f9cfaae406b1b13de1b
MohammadReza <mhmd.samadi76@gmail.com>
2018-07-14 00:10:21 +0430

    splash compelete

---
+++ app/component/Splash.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {Image, StyleSheet, View, Text, } from 'react-native';
+
+export default class Splash extends Component {
+  render() {
+    return (
+      <View style = {styles.mainPage}>
+         <View style = {styles.txtView}>
+            <Image  
+                styly = {styles.imgLogo} 
+                source={require('../assets/react.png')}/>
+            <Text style = {styles.txtTitle}>React-Native</Text>
+            <Text style = {styles.txtBody}>Simple Application for Test and Training</Text>
+         </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    mainPage : {
+        backgroundColor: "#043d61",
+        flex: 1,
+    },
+    txtTitle : {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color : "#5edbef",
+        opacity : 0.7
+    },
+    txtBody : {
+        fontSize: 14,
+        color : "#45b5c7",
+        marginTop: 2,
+        opacity : 0.6
+    },
+    txtView : {
+      flex : 0.8,
+      alignItems : 'center',
+      justifyContent: 'center',
+    },
+    imgLogo : {
+      width : 20,
+      height : 20, 
+    }
+
+});
