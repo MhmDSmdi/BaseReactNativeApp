3e9df52bdd80d37f5d87f679ebd80606f8c100cf
MhmdSmdi <mhmd.samadi76@gmail.com>
2018-07-21 12:53:35 +0430

    Swipe add to card

--- app/component/note_card/NoteCard.js
+++ app/component/note_card/NoteCard.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet , Alert} from 'react-native';
+import Swipeout from 'react-native-swipeout';
 
 class NoteCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeRowKey : null
+    };
+  }
   render() {
+    const swipeSetting = {
+      autoClose : true, 
+      onClose: (secId, rowId, direction) => {
+        this.setState({ activeRowKey : null});
+      },
+      onOpen: (secId, rowId, direction) => {
+        this.setState({ activeRowKey : this.props.item.key });
+      },
+      right : [
+        {
+          onPress : () => {
+            Alert.alert (
+              'Alert',
+              'Are yous sure you want to delete this card?',
+              [
+                {text : 'No', onPress : () => {}, style : 'cancel'},
+                {text : 'Yes', onPress : () => {
+                  
+                }, style : 'default'}
+              ],
+              {cancelable : true}
+            );
+          },
+          text : 'Delete', type : 'delete'
+        }
+      ],
+        rowId : this.props.index,
+        sectionId: 1
+    };
+    
     return (
-      <View style = {{backgroundColor: '#043d61', flex : 1, flexDirection : 'row'}}>
+     <Swipeout {...swipeSetting}>
+        <View style = {{backgroundColor: '#043d61', flex : 1, flexDirection : 'row'}}>
         <Image source = {{uri : this.props.item.imageUrl}} style = {{width : 100, height : 100, margin : 8}} />
         <View style = {{flex : 1, flexDirection : 'column'}}>
             <Text style = {styles.txtTitle}>{this.props.item.title}</Text>
@@ -15,7 +53,8 @@ class NoteCard extends Component {
                 {this.props.item.note}
             </Text>
         </View>
-      </View>
+        </View>
+     </Swipeout>
     );
   }
 }
--- app/data/noteListData.js
+++ app/data/noteListData.js
@@ -3,36 +3,49 @@
     // constructor() {
         var noteListData = [
             {
+                "key" : "1",
                 "title" : "title1",
                 "note" : "note1",
                 "imageUrl" : "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg"
             },
             {
+                "key" : "2",
+                "title" : "title1",
+                "note" : "note1",
+                "imageUrl" : "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg"
+            },
+            {
+                "key" : "3",
                 "title" : "title2",
-                "note" : "note2fodsjfopsdjfposdjafpodsfpodsjfopdsjfosdajfosjdfodsjfopjdopfjsdofjosapdjfpoasdjfposdjafposajdfopjsadofjsaodpfjosdapfjposadfjopsadjfopsdjfopsdjfopsajfopjsdopfjspdofjsopdafjopsdfjsopdajfposdajfopsajf",
+                "note" : "note2",
                 "imageUrl" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLrSBJSqyZuVQzkRL-GPhZNoqu57JJ7GY9KJwXy0SOZ0flkFr70A"
             },
             {
+                "key" : "4",
                 "title" : "title3",
                 "note" : "note3",
                 "imageUrl" : "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg"
             },
             {
+                "key" : "5",
                 "title" : "title3",
                 "note" : "note3",
                 "imageUrl" : "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg"
             },
             {
+                "key" : "6",
                 "title" : "title4",
                 "note" : "note4",
                 "imageUrl" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLrSBJSqyZuVQzkRL-GPhZNoqu57JJ7GY9KJwXy0SOZ0flkFr70A"
             },
             {
+                "key" : "7",
                 "title" : "title4",
                 "note" : "note4",
                 "imageUrl" : "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg"
             },
             {
+                "key" : "8",
                 "title" : "title5",
                 "note" : "note5",
                 "imageUrl" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLrSBJSqyZuVQzkRL-GPhZNoqu57JJ7GY9KJwXy0SOZ0flkFr70A"
